96ae6346bfb7de06423ec496b0dbfc3988c1f642
Panachai <panachai.ny@gmail.com>
2024-02-26 18:25:00 +0700

    update

--- feat/comfy_client/client.py
+++ feat/comfy_client/client.py
@@ -2,111 +2,20 @@ import json
 from urllib import request, parse
 import random
 
-# This is the ComfyUI api prompt format.
-
-# If you want it for a specific workflow you can "enable dev mode options"
-# in the settings of the UI (gear beside the "Queue Size: ") this will enable
-# a button on the UI to save workflows in api format.
-
-# keep in mind ComfyUI is pre alpha software so this format will change a bit.
-
-# this is the one for the default workflow
-prompt_text = """
-{
-    "3": {
-        "class_type": "KSampler",
-        "inputs": {
-            "cfg": 8,
-            "denoise": 1,
-            "latent_image": [
-                "5",
-                0
-            ],
-            "model": [
-                "4",
-                0
-            ],
-            "negative": [
-                "7",
-                0
-            ],
-            "positive": [
-                "6",
-                0
-            ],
-            "sampler_name": "euler",
-            "scheduler": "normal",
-            "seed": 8566257,
-            "steps": 20
-        }
-    },
-    "4": {
-        "class_type": "CheckpointLoaderSimple",
-        "inputs": {
-            "ckpt_name": "v1-5-pruned-emaonly.ckpt"
-        }
-    },
-    "5": {
-        "class_type": "EmptyLatentImage",
-        "inputs": {
-            "batch_size": 1,
-            "height": 512,
-            "width": 512
-        }
-    },
-    "6": {
-        "class_type": "CLIPTextEncode",
-        "inputs": {
-            "clip": [
-                "4",
-                1
-            ],
-            "text": "masterpiece best quality girl"
-        }
-    },
-    "7": {
-        "class_type": "CLIPTextEncode",
-        "inputs": {
-            "clip": [
-                "4",
-                1
-            ],
-            "text": "bad hands"
-        }
-    },
-    "8": {
-        "class_type": "VAEDecode",
-        "inputs": {
-            "samples": [
-                "3",
-                0
-            ],
-            "vae": [
-                "4",
-                2
-            ]
-        }
-    },
-    "9": {
-        "class_type": "SaveImage",
-        "inputs": {
-            "filename_prefix": "ComfyUI",
-            "images": [
-                "8",
-                0
-            ]
-        }
-    }
-}
-"""
-
 
 class ComfyClient:
 
     def queue_prompt(prompt):
-        p = {"prompt": prompt}
+        f = open('feat/comfy_client/workflows/default_workflow_api.json')
+        data = json.load(f)
+
+        p = {"prompt": data}
         data = json.dumps(p).encode('utf-8')
-        req = request.Request("http://127.0.0.1:8188/prompt", data=data)
+        # print(data)
+        req = request.Request(
+            "http://127.0.0.1:8188/prompt", data=data, method='POST')
+        y = json.dumps(req)
+        print(y)
         request.urlopen(req)
 
 
--- makefile
+++ makefile
@@ -8,7 +8,7 @@ setup:
 gen_dependency:
 	# pipreqs .
 	# pipreqsnb .
-	
+
 i: install
 install:
 	python -m pip install -r requirements.txt
@@ -22,5 +22,5 @@ clean:
 	pip freeze | while read p; do pip uninstall -y "$p"; done
 
 dev:
-	uvicorn main:app --host 0.0.0.0 --port 5000
+	uvicorn main:app --reload --host 0.0.0.0 --port 5000
 	# uvicorn main:app --reload --port 5000
