26ee07dad57a68deddb35c87531511643757322e
panachainy <panachai.ny@gmail.com>
2024-02-28 11:00:33 +0700

    1

---
+++ ai-qa-2/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('🎈 App Name')
+
+st.write('Hello world!')
+st.write('Hello world!')
+
+import streamlit as st
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
