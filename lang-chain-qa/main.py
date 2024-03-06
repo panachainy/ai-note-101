9bed28010783dde1fb9fd2cf389dab15ec53c1dc
Panachai <panachai.ny@gmail.com>
2024-03-06 15:55:12 +0700

    worked

--- lang-chain-qa/main.py
+++ lang-chain-qa/main.py
@@ -2,11 +2,18 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import getpass
 import os
 from langchain_community.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
-import sys
-from langchain_google_genai import GoogleGenerativeAI, HarmBlockThreshold, HarmCategory
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# import sys
+from langchain_google_genai import HarmBlockThreshold, HarmCategory
 from langchain.prompts import PromptTemplate
 from langchain_core.documents import Document
+from langchain.chains import RetrievalQA
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# import chromadb
+# from chromadb.utils import embedding_functions
+import chromadb.utils.embedding_functions as embedding_functions
+
 from typing import (
     List,
 )
@@ -29,13 +36,26 @@ api_key = config["GOOGLE_API_KEY"]
 #                          },
 #                          )
 
-llm = GoogleGenerativeAI(
-    model="gemini-pro",
-    google_api_key=api_key,
-    safety_settings={
-        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-    },
-)
+
+# ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0,
+                             top_p=0.85,
+                             #  model="gemini-pro",
+                             google_api_key=api_key,
+                             safety_settings={
+                                 HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+                             },
+                             # FIXME: not sure
+                             convert_system_message_to_human=True
+                             )
+# llm = GoogleGenerativeAI(
+#     model="gemini-pro",
+#     google_api_key=api_key,
+#     safety_settings={
+#         HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+#     },
+# )
 
 
 def ex_chat(question: str):
@@ -97,19 +117,22 @@ def load_docs_to_splitter(docs: list) -> List[Document]:
 
 
 def get_vectordb() -> any:
+    persist_directory = 'docs/chroma/'
+    # embedding_function = embedding_functions.GoogleGenerativeAiEmbeddingFunction(
+    #     api_key=api_key
+    # )
     embedding = GoogleGenerativeAIEmbeddings(
         model="models/embedding-001",
         google_api_key=api_key,
         # task_type="retrieval_query",
         # task_type="retrieval_document"
     )
-
-    persist_directory = 'docs/chroma/'
-
     # check if have chroma in local need to skip `Chroma.from_documents` use `Chroma` instead
     if os.path.exists(persist_directory+'chroma.sqlite3'):
         return Chroma(
-            persist_directory=persist_directory
+            persist_directory=persist_directory,
+            embedding_function=embedding,
+            # embedding_function=embedding_function
         )
     else:
         docs = multiple_pdf_load(
@@ -119,21 +142,34 @@ def get_vectordb() -> any:
         return Chroma.from_documents(
             documents=splits,
             embedding=embedding,
+            # embedding_function=embedding_function,
             persist_directory=persist_directory
         )
 
 
-# Create the vector store (only first time)
-# vectordb = Chroma.from_documents(
-#     documents=splits,
-#     embedding=embedding,
-#     persist_directory=persist_directory
-# )
-# vectordb = Chroma(
-#     persist_directory=persist_directory,
-#     embedding_function=embedding
-# )
-
 vectordb = get_vectordb()
 
 print(vectordb._collection.count())
+print(vectordb._collection)
+# print(vectordb._collection.find_one())
+# question = "what did they say about matlab?"
+# docs_ss = vectordb.similarity_search(question, k=3)
+# docs_ss[0].page_content[:100]
+# docs_ss[1].page_content[:100]
+
+# docs_mmr = vectordb.max_marginal_relevance_search(question, k=3)
+# docs_mmr[0].page_content[:100]
+# docs_mmr[1].page_content[:100]
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever()
+)
+
+# Pass question to the qa_chain
+question = "What is AI?"
+result = qa_chain({"query": question})
+result["result"]
+
+print(result)
+# vectordb
